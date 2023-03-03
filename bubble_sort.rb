3c9983c0a6af214609fbd6160dfe6fff1c0fc913
VladMahdenko <mahdenko2020ks13@student.karazin.ua>
2023-03-03 15:05:43 +0200

    implemented bubble sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,2 +1,19 @@
 class BubbleSort
+  def self.sort(array)
+    swap = true
+    while swap
+      swap = false
+      (array.length-1).times do |i|
+        if array[i]>array[i+1]
+          array[i], array[i+1] = array[i+1], array[i]
+          swap = true
+        end
+      end
+    end
+  end
 end
+
+arr = Array[4, 3, 2, 1]
+puts "#{arr}"
+BubbleSort.sort(arr)
+puts "#{arr}"
